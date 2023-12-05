04f693d0de8b9ac3d1c7c3b6cde06b5baf3aed65
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-05 17:28:59 +0100

    Import angular material toolbarmodule in shared.module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OmdbService } from './services/omdb/omdb.service';
 import { StorageService } from './services/storage/storage.service';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ],
   providers: [
     OmdbService,
     StorageService
-  ]
+  ],
+  exports: [
+    MatToolbarModule
+  ]  
 })
 export class SharedModule { }
