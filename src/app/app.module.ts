8cd982c0d6aa3333fc3f8522c531ade7158ca585
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-06 11:37:34 +0100

    Create omdb service: - Import httpClient - Create basic call to api - No pipe yet to handdle response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';  
 
@@ -20,6 +21,7 @@ import { AppComponent } from './app.component';
     CoreModule,
     SharedModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/services/omdb/omdb.service.ts
+++ src/app/shared/services/omdb/omdb.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies(title:string, type:string, year:string):Observable<any>{
+    return this.http.get<any>(environment.baseUrlOmdb, {
+      params:{
+        apikey: environment.apiKeyOmdb,
+        s: title,
+        type: type,
+        year: year
+      }
+    })
+  }
 }
