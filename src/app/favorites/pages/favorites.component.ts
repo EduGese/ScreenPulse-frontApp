90a4925fc17b4d135ca8e6398963c4a204667c3f
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-07 18:06:29 +0100

    Get favorites movies in favorites component

--- src/app/favorites/pages/favorites.component.ts
+++ src/app/favorites/pages/favorites.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent {
+export class FavoritesComponent implements OnInit{
+
+  constructor(private storageService: StorageService){}
+  ngOnInit(): void {
+    console.log(this.storageService.getFavorites());
+  }
 
 }
