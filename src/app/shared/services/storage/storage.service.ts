7ae9f4c19e48e133d81c87434eff26d8822c70db
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-07 17:39:22 +0100

    Implement function getFavorites in storage service

--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -2,20 +2,26 @@ import { Injectable } from '@angular/core';
 import { Movie } from '../../models/movie.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
   favorites: Movie[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  addToFavories(movie:Movie){
+  addToFavories(movie: Movie) {
     this.favorites.push(movie);
     const favoritesString = JSON.stringify(this.favorites);
-    
-    localStorage.setItem('favorites', favoritesString)
-    
+    localStorage.setItem('favorites', favoritesString);
+  }
+  getFavorites() {
+    const favoritesString = localStorage.getItem('favorites');
+    if(favoritesString != null){
+      const favorites = JSON.parse(favoritesString);
+      return favorites;
+    }else{
+      return null;
     }
-
+  }
 
 }
