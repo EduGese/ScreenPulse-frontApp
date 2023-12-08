3afc6306dd5e7d14d9ca04eb1c8732b7706531e7
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-08 20:46:56 +0100

    Create function in favorites-card component: - Get current list of favorites - Filter movie with given imdbID - Pass updated array to storage service - Storage service saves updated favorites array

--- src/app/favorites/components/favorites-card/favorites-card.component.ts
+++ src/app/favorites/components/favorites-card/favorites-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Movie } from 'src/app/shared/models/movie.model';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
 
 @Component({
   selector: 'app-favorites-card',
@@ -8,4 +9,12 @@ import { Movie } from 'src/app/shared/models/movie.model';
 })
 export class FavoritesCardComponent {
   @Input () favorites!:Movie[];
+
+  constructor(private storageService: StorageService){}
+
+
+  deleteFavorite(imdbID: string){
+    const favorites = this.storageService.getFavorites();
+    this.storageService.deleteMovie(favorites.filter(movie => movie.imdbID !== imdbID))
+  }
 }
