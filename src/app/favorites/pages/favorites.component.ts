fb998e95c776f3fc3ca2ea9cf62a6c64241573e6
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-09 14:20:01 +0100

    Delete favoritesSubscription: Subscription; variable as will not be necesary the subscrition related anywhere else in the component

--- src/app/favorites/pages/favorites.component.ts
+++ src/app/favorites/pages/favorites.component.ts
@@ -6,53 +6,47 @@ import { StorageService } from 'src/app/shared/services/storage/storage.service'
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
 })
-export class FavoritesComponent implements OnInit{
+export class FavoritesComponent implements OnInit {
   favorites: Movie[] | [] = [];
-  title: string ='';
+  title: string = '';
   type: string = '';
   year: string = '';
 
-  favoritesSubscription: Subscription;
+  constructor(private storageService: StorageService) {
 
-  constructor(private storageService: StorageService){
-    this.favoritesSubscription = this.storageService.favoritesAfterDeleteMovie
-      .subscribe(favorites => {
-        this.favorites = favorites;   
-     });
-    
-    this.storageService.favoritesAfterUpdateMovie.subscribe(movie => {
+    this.storageService.favoritesAfterDeleteMovie.subscribe((favorites) => {
+      this.favorites = favorites;
+    });
 
-      const index = this.favorites.findIndex(m => m.imdbID == movie.imdbID);
-    
+    this.storageService.favoritesAfterUpdateMovie.subscribe((movie) => {
+      const index = this.favorites.findIndex((m) => m.imdbID == movie.imdbID);
       this.favorites[index] = movie;
-    
-    })
-
+    });
   }
+  
   ngOnInit(): void {
     this.favorites = this.storageService.getFavorites();
-    console.log(this.favorites)
+    console.log(this.favorites);
   }
 
-  onSubmit(){
+  onSubmit() {
     this.favorites = this.storageService.getFavorites();
-    if(!this.favorites || this.favorites.length === 0) {
-      console.log('No favorites saved!'); 
+    if (!this.favorites || this.favorites.length === 0) {
+      console.log('No favorites saved!');
     } else {
       let filteredFavorites = this.storageService.getFavorites();
 
-    
-     if (this.year) {
+      if (this.year) {
         filteredFavorites = filteredFavorites.filter(
           (movie) => movie.Year === this.year
         );
-        if(filteredFavorites.length>0){
+        if (filteredFavorites.length > 0) {
           this.storageService.addToFilterdFavories(filteredFavorites);
         }
       }
-   
+
       if (this.type !== 'all') {
         filteredFavorites = filteredFavorites.filter(
           (movie) => movie.Type === this.type
@@ -60,14 +54,12 @@ export class FavoritesComponent implements OnInit{
         this.storageService.addToFilterdFavories(filteredFavorites);
       }
 
-    
       if (this.title) {
         filteredFavorites = filteredFavorites
           .filter((movie) =>
             movie.Title.toLowerCase().startsWith(this.title.toLowerCase())
           )
           .map((movie) => {
-          
             const words = movie.Title.split(' ');
 
             const capitalizedWords = words.map((word) => {
@@ -81,9 +73,9 @@ export class FavoritesComponent implements OnInit{
         this.storageService.addToFilterdFavories(filteredFavorites);
       }
 
-      if(filteredFavorites.length === 0){
+      if (filteredFavorites.length === 0) {
         this.favorites = [];
-      }else{
+      } else {
         this.favorites = filteredFavorites;
       }
     }
