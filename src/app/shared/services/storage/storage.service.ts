3254090f5807cd2a4ef61e57fdd5b6de24f04bd4
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-09 17:25:46 +0100

    Add duplicate checking and toasts when adding to favorites:  - Verify duplicates before add to favorites  - Add success or error toast message

--- src/app/shared/services/storage/storage.service.ts
+++ src/app/shared/services/storage/storage.service.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from 'ngx-toastr';
 import { Injectable } from '@angular/core';
 import { Movie } from '../../models/movie.model';
 import { Subject } from 'rxjs';
@@ -12,15 +13,22 @@ export class StorageService {
   favoritesAfterDeleteMovie = new Subject<Movie[]>();
   favoritesAfterUpdateMovie = new Subject<Movie>();
 
-  constructor() {}
+  constructor(private toastrService: ToastrService) {}
 
   addToFavories(movie: Movie) {
-    this.favorites = this.getFavorites();
-    this.favorites.push(movie);
-    const favoritesString = JSON.stringify(this.favorites);
-    localStorage.setItem('favorites', favoritesString);
+    const favorites = this.getFavorites();
+    const favoriteDuplicate = favorites.find((m) => m.imdbID === movie.imdbID);
+    if (favoriteDuplicate) {
+      this.toastrService.error(movie.Title, 'It was already in your list')
+    } else {
+      this.favorites = this.getFavorites();
+      this.favorites.push(movie);
+      const favoritesString = JSON.stringify(this.favorites);
+      localStorage.setItem('favorites', favoritesString);
+      this.toastrService.success(movie.Title, 'Added to favorites')
+    }
   }
-  getFavorites():Movie[] | [] {
+  getFavorites(): Movie[] | [] {
     const favoritesString = localStorage.getItem('favorites');
     return favoritesString ? JSON.parse(favoritesString) : [];
   }
@@ -30,26 +38,25 @@ export class StorageService {
     const filteredFavoritesString = JSON.stringify(this.filterdFavorites);
     localStorage.setItem('filteredFavorites', filteredFavoritesString);
   }
-  getFilteredFavorites():Movie[] | [] {
+  getFilteredFavorites(): Movie[] | [] {
     const filteredFavoritesString = localStorage.getItem('filteredFavorites');
     return filteredFavoritesString ? JSON.parse(filteredFavoritesString) : [];
   }
-  deleteMovie(moviesUpdated: Movie[]){
-    const moviesAfterDelete= moviesUpdated;
+  deleteMovie(moviesUpdated: Movie[]) {
+    const moviesAfterDelete = moviesUpdated;
     localStorage.setItem('favorites', JSON.stringify(moviesAfterDelete));
     this.favoritesAfterDeleteMovie.next(this.getFavorites());
   }
-  addReview(description: string, imdbID: string){
+  addReview(description: string, imdbID: string) {
     const favorites = this.getFavorites();
-    const movieToUpdate = favorites.find(movie => movie.imdbID === imdbID);
-    if(!movieToUpdate) {
-      throw new Error('Could not find movie to update'); 
-    }else{
+    const movieToUpdate = favorites.find((movie) => movie.imdbID === imdbID);
+    if (!movieToUpdate) {
+      throw new Error('Could not find movie to update');
+    } else {
       movieToUpdate.description = description;
-      const updatedMovie = {...movieToUpdate, description}; 
+      const updatedMovie = { ...movieToUpdate, description };
       localStorage.setItem('favorites', JSON.stringify(favorites));
       this.favoritesAfterUpdateMovie.next(updatedMovie);
     }
-
   }
 }
