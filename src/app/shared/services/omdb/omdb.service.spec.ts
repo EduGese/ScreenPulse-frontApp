2325f947b28132f330f2d51baee3ebb134c17ef1
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-11 00:38:35 +0100

    test: add HttpClientModule import
    Import HttpClientModule in test setup to provide HttpClient dependency for OmdbService. This fixes the No provider for HttpClient! error.

--- src/app/shared/services/omdb/omdb.service.spec.ts
+++ src/app/shared/services/omdb/omdb.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OmdbService } from './omdb.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('OmdbService', () => {
   let service: OmdbService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(OmdbService);
   });
 
