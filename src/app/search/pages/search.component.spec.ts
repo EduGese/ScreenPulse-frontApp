c19753f981be3c930fd32cf7b772a5a4b8057aa6
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-11 12:28:35 +0100

    Fix unit test errors in favorites.component, add necesaries:  - Declarations  - Imports  - Providers

--- src/app/search/pages/search.component.spec.ts
+++ src/app/search/pages/search.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
+import { OmdbService } from 'src/app/shared/services/omdb/omdb.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -8,7 +21,22 @@ describe('SearchComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchComponent]
+      declarations: [SearchComponent, NavbarComponent],
+      providers: [OmdbService, ToastrService],
+      imports: [
+        HttpClientModule,
+        ToastrModule.forRoot({}),
+        MatIconModule,
+        MatFormFieldModule,
+        MatToolbarModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        MatGridListModule
+
+      ],
     });
     fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
