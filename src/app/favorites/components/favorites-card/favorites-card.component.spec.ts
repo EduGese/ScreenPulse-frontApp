714f45d7b9062c35637f12c503f7edeb1a6b9f98
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-11 12:30:06 +0100

    Fix unit test errors in favorites.card-component, add necesaries:  - Declarations  - Imports  - Providers

--- src/app/favorites/components/favorites-card/favorites-card.component.spec.ts
+++ src/app/favorites/components/favorites-card/favorites-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesCardComponent } from './favorites-card.component';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 describe('FavoritesCardComponent', () => {
   let component: FavoritesCardComponent;
@@ -8,7 +10,13 @@ describe('FavoritesCardComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FavoritesCardComponent]
+      declarations: [FavoritesCardComponent],
+      providers: [StorageService,
+        ToastrService],
+        imports: [
+          ToastrModule.forRoot({
+          })
+        ]
     });
     fixture = TestBed.createComponent(FavoritesCardComponent);
     component = fixture.componentInstance;
