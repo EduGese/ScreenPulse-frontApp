50fdac0f3e980b21394a8c09b49f9cddb325c1b4
EduGese <122921699+EduGese@users.noreply.github.com>
2023-12-11 12:26:40 +0100

    Fix unit test errors in favorites.component, add necesaries:  - Declarations  - Imports  - Providers

--- src/app/favorites/pages/favorites.component.spec.ts
+++ src/app/favorites/pages/favorites.component.spec.ts
@@ -1,6 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesComponent } from './favorites.component';
+import { FormsModule } from '@angular/forms';
+import { StorageService } from 'src/app/shared/services/storage/storage.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { FavoritesFilterService } from '../services/favoritesFilterService/favorites-filter.service';
+import { NavbarComponent } from 'src/app/shared/components/navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FavoritesCardComponent } from '../components/favorites-card/favorites-card.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
@@ -8,7 +21,23 @@ describe('FavoritesComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FavoritesComponent]
+      declarations: [
+        FavoritesComponent,
+        NavbarComponent,
+        FavoritesCardComponent,
+      ],
+      imports: [
+        FormsModule,
+        ToastrModule.forRoot(),
+        MatIconModule,
+        MatFormFieldModule,
+        MatToolbarModule,
+        MatInputModule,
+        MatSelectModule,
+        MatButtonModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [StorageService, ToastrService, FavoritesFilterService],
     });
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
