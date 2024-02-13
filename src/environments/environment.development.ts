16aed457d564d3a74e5f0b0e335c8d897ad702d5
EduGese <122921699+EduGese@users.noreply.github.com>
2024-02-13 19:29:19 +0100

    Style: change style of editing view in cards

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
     baseUrlOmdb : 'http://www.omdbapi.com/',
     apiKeyOmdb : '1cedb25f',
     serverFavoritesURL : 'http://localhost:9000/api/favorites',
-    serverSearchURL : 'http://localhost:9000/api2/omdb',
+    serverSearchURL : 'http://localhost:9000/api/omdb',
 
 };
