4f7f93fd9cdf46134331df9b800610fe6bc0cea0
EduGese <122921699+EduGese@users.noreply.github.com>
2024-02-29 17:52:07 +0100

    Change table logic and style: - Change table component logic to make it less abstract - Applied some styles

--- src/app/pages/search/page/search.component.ts
+++ src/app/pages/search/page/search.component.ts
@@ -19,7 +19,79 @@ export class SearchComponent {
   title: string ='';
   type: string = 'movie';
   year: string = '';
-  results: Movie[] = [];
+  // results: Movie[] = [];
+  results: Movie[] |Object[] = [
+    {
+      Title: "Rambo",
+      Year: "2008",
+      imdbID: "tt0462499",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BMTI5Mjg1MzM4NF5BMl5BanBnXkFtZTcwNTAyNzUzMw@@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo: First Blood Part II",
+      Year: "1985",
+      imdbID: "tt0089880",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BZWFkY2I1ZDAtNmZhNS00NjVlLWJiMGQtMGQ1ZmM0ZDA5ODg5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo III",
+      Year: "1988",
+      imdbID: "tt0095956",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOTIwNWJhZDItZmNmOC00M2NkLWIwNDktMTYwZWFlZDVkMmVkL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo: Last Blood",
+      Year: "2019",
+      imdbID: "tt1206885",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNTAxZWM2OTgtOTQzOC00ZTI5LTgyYjktZTRhYWM4YWQxNWI0XkEyXkFqcGdeQXVyMjMwNDgzNjc@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo",
+      Year: "1986",
+      imdbID: "tt0222619",
+      Type: "series",
+      Poster: "https://m.media-amazon.com/images/M/MV5BZDQ0M2M2MjktMmViYy00MDM5LWE1NWEtZmRhNzZmMGM3MzkxXkEyXkFqcGdeQXVyMjY3MjUzNDk@._V1_SX300.jpg"
+  },
+  {
+      Title: "Arthur Rambo",
+      Year: "2021",
+      imdbID: "tt10951972",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNzg5ZjJiNWEtNzhiZC00NzNhLThjZDgtZGU4OTMwN2I5YWY1XkEyXkFqcGdeQXVyNDgzNjg5Nw@@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo III",
+      Year: "1989",
+      imdbID: "tt0301766",
+      Type: "game",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNDE3Y2NkODgtMzhmNi00M2M3LTgxMTAtNjBhNTJiOTdmZDIzXkEyXkFqcGdeQXVyMjY3MjUzNDk@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo: First Blood Part II",
+      Year: "1986",
+      imdbID: "tt0301768",
+      Type: "game",
+      Poster: "https://m.media-amazon.com/images/M/MV5BOWUzMDE1NTktMTU0OS00NTE3LWE2NzItMzA3MGM2NzdkYTJlXkEyXkFqcGdeQXVyMjY3MjUzNDk@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo",
+      Year: "2012",
+      imdbID: "tt3107798",
+      Type: "movie",
+      Poster: "https://m.media-amazon.com/images/M/MV5BNDUwOGYwYWUtZjMzNi00MDIwLWE2NjgtYTdhODJmMTQwMGMyXkEyXkFqcGdeQXVyMzQzMDc2MDk@._V1_SX300.jpg"
+  },
+  {
+      Title: "Rambo",
+      Year: "1987",
+      imdbID: "tt0301765",
+      Type: "game",
+      Poster: "https://m.media-amazon.com/images/M/MV5BYmMxZGVjYzYtNWY4ZC00ZjAwLWFjNWQtOWFjNGEzNDQ2ZDM3XkEyXkFqcGdeQXVyMTgwOTE5NDk@._V1_SX300.jpg"
+  }
+  ];
 
   types:any[] = [
     {value: 'movie', viewValue: 'Movie'},
@@ -35,6 +107,7 @@ export class SearchComponent {
     { property: 'Poster', header: 'Poster'},
     { property: 'Add', header: 'Add'}
  ];
+
 
   constructor(
     private OmdbService: OmdbService, 
@@ -63,6 +136,7 @@ export class SearchComponent {
     this.OmdbService.getMovies(title, type, year).subscribe(
       (response)=>{
         //Handdle responses with no movies
+        console.log('Searched movies',response)
         response.Error ? this.toastrService.error(response.Error, 'Search error') : this.results = response.Search || [];// asign [] in case response is undefined
         type = type == '' ? 'all' : type;
       },
--- src/app/shared/components/movie-results-table/movie-results-table.component.ts
+++ src/app/shared/components/movie-results-table/movie-results-table.component.ts
@@ -1,32 +1,20 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { TableColumn } from 'src/app/shared/models/tableColumn.model';
+import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+
 
 @Component({
   selector: 'app-movie-results-table',
   templateUrl: './movie-results-table.component.html',
   styleUrls: ['./movie-results-table.component.css'],
 })
-export class MovieResultsTableComponent implements OnInit {
+export class MovieResultsTableComponent  {
   @Input() results!: any[];
-  @Input() columns: TableColumn[] = [];
   @Output() addItem = new EventEmitter<any>();
 
-  displayedColumns: string[] = [];
+  displayedColumns: string[] =  ['Title', 'Year', 'Type', 'Poster', 'Add' ];
   
   constructor() {}
 
-  ngOnInit(): void {
-    this.columns.forEach((element) => {
-      this.displayedColumns.push(element.header);
-    });
-  }
 
-  isAnImage(property: string): boolean {
-    if (property.endsWith('.jpg') || property === 'N/A') {
-      return true;
-    } else 
-    return false;
-  }
   addNewItem(item: any) {
     
     this.addItem.emit(item);
