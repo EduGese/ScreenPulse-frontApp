112bd063414edf522e50d84979a854618b7e4fe6
EduGese <122921699+EduGese@users.noreply.github.com>
2024-03-18 14:46:59 +0100

    Set app config to production

---
+++ src/environments/environments.production.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    baseUrlOmdb : 'http://www.omdbapi.com/',
+    apiKeyOmdb : '1cedb25f',
+    serverFavoritesURL: 'https://screenpulse-api.onrender.com/api/favorites',
+    serverSearchURL: 'https://screenpulse-api.onrender.com/api/omdb',
+    serverUserURL: 'https://screenpulse-api.onrender.com/api/user'
+
+};
