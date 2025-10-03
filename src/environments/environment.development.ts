8fa3b8b2e28d93910694cca1ae3110bda4900fce
EduGese <122921699+EduGese@users.noreply.github.com>
2025-10-03 20:12:56 +0200

    fix: remove unused baseUrlOmdb from environment files
    - Removed baseUrlOmdb from environment.development.ts
    - Removed baseUrlOmdb from environment.ts
    - Removed baseUrlOmdb from environments.production.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    baseUrlOmdb : 'http://www.omdbapi.com/',
     serverFavoritesURL : 'http://localhost:9000/api/favorites',
     serverSearchURL : 'http://localhost:9000/api/omdb',
     serverUserURL : 'http://localhost:9000/api/user'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    baseUrlOmdb : 'http://www.omdbapi.com/',
     serverFavoritesURL: 'https://screenpulse-api.onrender.com/api/favorites',
     serverSearchURL: 'https://screenpulse-api.onrender.com/api/omdb',
     serverUserURL: 'https://screenpulse-api.onrender.com/api/user'
