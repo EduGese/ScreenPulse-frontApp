fcb7f1107c079f9c88425c2db9e3852d0bedf74f
EduGese <122921699+EduGese@users.noreply.github.com>
2025-11-06 12:12:59 +0100

    refactor(auth): use AuthUser for response types

--- src/app/pages/auth/components/auth-form/auth-form.component.ts
+++ src/app/pages/auth/components/auth-form/auth-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { User } from 'src/app/shared/models/user.model';
+import { User } from 'src/app/shared/models/auth.model';
 
 
 /**
